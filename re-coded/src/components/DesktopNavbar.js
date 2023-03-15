226ad3cad7e20f05f65dd1d32061b933a38af822
felixloot <felix.bakkum2002@gmail.com>
2023-03-15 21:02:49 +0100

    Recode start

---
+++ re-coded/src/components/DesktopNavbar.js
@@ -0,0 +1,57 @@
+export default function DesktopNavbar() {
+  return (
+    <div>
+      <nav className="relative px-4 py-4 flex justify-between items-center bg-white">
+        <a className="text-3xl font-bold leading-none" href="#">
+          Felix
+        </a>
+        <div className="lg:hidden">
+          <button className="navbar-burger flex items-center text-blue-600 p-3">
+            <svg
+              className="block h-4 w-4 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Mobile menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+            </svg>
+          </button>
+        </div>
+        <ul className="hidden lg:flex lg:items-center lg:w-auto gap-8">
+          <li>
+            <a className="text-sm text-gray-400 hover:text-gray-500" href="#">
+              Hem
+            </a>
+          </li>
+          <li>
+            <a className="text-sm text-gray-400 hover:text-gray-500" href="#">
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a className="text-sm text-gray-400 hover:text-gray-500" href="#">
+              Rekommendationer
+            </a>
+          </li>
+          <li>
+            <a className="text-sm text-gray-400 hover:text-gray-500" href="#">
+              Kontakt
+            </a>
+          </li>
+        </ul>
+        {/* <a
+          className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200"
+          href="#"
+        >
+          Sign In
+        </a>
+        <a
+          className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200"
+          href="#"
+        >
+          Sign up
+        </a> */}
+      </nav>
+    </div>
+  );
+}
---
+++ re-coded/src/components/sections/Hero.js
@@ -0,0 +1,19 @@
+export default function Hero() {
+  return (
+    <div className="flex">
+      <div>
+        <h1>Teknikentusiast</h1>
+        <p>
+          Hej ! Jag är en 21-årig kille från Göteborg som har varit inne på
+          programmeringsspåret sedan jag var 15 år. Jag har specialiserat mig på
+          frontend-utveckling och har en passion för att utforska nya tekniker
+          och verktyg. Min tekniska nyfikenhet och snabba inlärningsförmåga gör
+          mig till en skicklig utvecklare med öga för detaljer.
+        </p>
+        <a></a>
+        <div>icon icon icon</div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
